c91b4b020e4503cb0aa72711fa41cb53661a7c79
Ayşegül Bozdağ <aysegulbozdag4@gmail.com>
2023-01-17 23:35:49 +0300

    Code refactored.

--- app/src/main/java/com/example/rickandmorty/di/NetworkModule.kt
+++ app/src/main/java/com/example/rickandmorty/di/NetworkModule.kt
@@ -1,4 +1,22 @@
 package com.example.rickandmorty.di
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Provides
+import okhttp3.Interceptor
+import javax.inject.Singleton
+
 class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun getGson() :Gson = GsonBuilder().serializeNulls().setLenient().create()
+
+    @Provides
+    @Singleton
+    fun getInterceptor() : Interceptor = Interceptor{
+        val request = it.request().newBuilder()
+        val actualRequest  = request.build()
+        it.proceed(actualRequest)
+    }
 }
